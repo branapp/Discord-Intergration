c8bfbc8c8cac297d0bfe25c3d1192c3fe03cff11
ooTruffle <Truffle_YT@hotmail.com>
2024-05-05 22:35:42 +1200

    Fix .gitignore, Fix bot Image

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on(Events.InteractionCreate, async interaction => {
 
 client.once(Events.ClientReady, readyClient => {
 	logger.info(`Logged in as ${client.user.tag}!`);
-	client.user.setAvatar('../public/assets/img/bran.png');
+	client.user.setAvatar('Images/bran.png');
 	client.user.setActivity('Bran');
 });
 
